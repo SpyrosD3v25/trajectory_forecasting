a457d5afea787aad6e7acde46be8257aee361f68
SpyrosD3v25 <code123go@gmail.com>
2026-07-05 18:46:02 +0300

    test_kinematic_chart(feat) add module imports

---
+++ tests/test_kinematic_chart.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+from kineroute_nvp.geometry.kinematics import (
+    chart_to_positions_numpy,
+    decode_chart_numpy,
+    encode_positions_numpy,
+    positions_to_chart_numpy,
+)
+
+
