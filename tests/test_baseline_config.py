d0bf313aa454d1a962970670d800be4d54667277
SpyrosD3v25 <code123go@gmail.com>
2026-07-05 18:46:02 +0300

    test_baseline_config(test) add test_baseline_yaml_parsing coverage

--- tests/test_baseline_config.py
+++ tests/test_baseline_config.py
@@ -1,3 +1,13 @@
 from kineroute_nvp.utils.baseline_config import args_to_config, parse_args
 
 
+def test_baseline_yaml_parsing() -> None:
+    args = parse_args(["--config", "experiments/baselines/gru_coords_paper.yaml"])
+    config = args_to_config(args)
+    assert args.experiment_name == "baselines"
+    assert config["run_name"] == "gru_coords_paper"
+    assert config["model"]["name"] == "gru"
+    assert config["data"]["raw_roots"] == [
+        "data/envship/paper_dma_clean_ship_core_lite_v1",
+        "data/envship/paper_noaa_clean_ship_core_lite_v1",
+    ]
