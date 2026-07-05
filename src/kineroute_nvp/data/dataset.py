9fd7384320dc9b458b3348ddcc9a7892dedaee34
SpyrosD3v25 <code123go@gmail.com>
2026-07-05 18:45:58 +0300

    dataset(feat) add module imports

---
+++ src/kineroute_nvp/data/dataset.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+import json
+from pathlib import Path
+from typing import Any, Dict
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+
